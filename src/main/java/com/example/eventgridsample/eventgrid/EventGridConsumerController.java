343ced7b99bfb8f9af5aadcfd9a05ce414f921b7
Rahul Nehra <60338882+rahulnehra477@users.noreply.github.com>
2023-11-06 16:16:00 +0530

    Update EventGridConsumerController.java

--- src/main/java/com/example/eventgridsample/eventgrid/EventGridConsumerController.java
+++ src/main/java/com/example/eventgridsample/eventgrid/EventGridConsumerController.java
@@ -2,6 +2,7 @@ package com.example.eventgridsample.eventgrid;
 
 import com.google.gson.Gson;
 import com.microsoft.azure.eventgrid.models.SubscriptionValidationResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Iterator;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class EventGridConsumerController {
@@ -17,28 +19,28 @@ public class EventGridConsumerController {
     @PostMapping("/validate")
     public ResponseEntity<?> validate(@RequestBody List<Event> body, @RequestHeader HttpHeaders headers) {
 
-        System.out.println("In Validate Function");
+        log.info("In Validate Function");
 
         Iterator itr = headers.keySet().iterator();
 
         while (itr.hasNext()) {
             String key = itr.next().toString();
-            System.out.println("Key: " + key + ", Value: " + headers.get(key));
+            log.info("Key: " + key + ", Value: " + headers.get(key));
         }
 
         Gson g = new Gson();
 
-        System.out.println("JSON: " + g.toJson(body));
+        log.info("JSON: " + g.toJson(body));
 
         boolean isValidationEvent = body.get(0).getEventType().equalsIgnoreCase("Microsoft.EventGrid.SubscriptionValidationEvent");
 
 
         if (isValidationEvent) {
             String validationCode = g.toJsonTree(body).getAsJsonArray().get(0).getAsJsonObject().getAsJsonObject("data").getAsJsonObject().get("validationCode").getAsString();
-            System.out.println("Validation Event");
+            log.info("Validation Event");
             SubscriptionValidationResponse subscriptionValidationResponse = new SubscriptionValidationResponse();
             subscriptionValidationResponse.withValidationResponse(validationCode);
-            System.out.println("Validation Response: " + subscriptionValidationResponse.validationResponse());
+            log.info("Validation Response: " + subscriptionValidationResponse.validationResponse());
             return ResponseEntity.ok(subscriptionValidationResponse);
         }
 
@@ -46,25 +48,31 @@ public class EventGridConsumerController {
     }
 
     @PostMapping("/subscribe")
-    public ResponseEntity<?> subscribe(@RequestBody List<Event> body, @RequestHeader HttpHeaders headers){
+    public ResponseEntity<?> subscribe(@RequestBody List<Event> body, @RequestHeader HttpHeaders headers, @RequestHeader(value = "supplierName") String supplierName){
 
-        System.out.println("In subscribe Function");
+        log.info("In subscribe Function");
+        log.info("header supplierName" + supplierName);
 
         printHeaders(headers);
 
         Gson g = new Gson();
 
-        System.out.println("JSON: " + g.toJson(body));
+        log.info("JSON: " + g.toJson(body));
 
         boolean isValidationEvent = body.get(0).getEventType().equalsIgnoreCase("Microsoft.EventGrid.SubscriptionValidationEvent");
-
+        boolean isCreateEvent = body.get(0).getEventType().equalsIgnoreCase("Microsoft.Storage.BlobCreated");
+        boolean isDeleteEvent = body.get(0).getEventType().equalsIgnoreCase("Microsoft.Storage.BlobDeleted");
         if (isValidationEvent) {
             String validationCode = g.toJsonTree(body).getAsJsonArray().get(0).getAsJsonObject().getAsJsonObject("data").getAsJsonObject().get("validationCode").getAsString();
-            System.out.println("Validation Event");
+            log.info("Validation Event");
             SubscriptionValidationResponse subscriptionValidationResponse = new SubscriptionValidationResponse();
             subscriptionValidationResponse.withValidationResponse(validationCode);
-            System.out.println("Validation Response: " + subscriptionValidationResponse.validationResponse());
+            log.info("Validation Response: " + subscriptionValidationResponse.validationResponse());
             return ResponseEntity.ok(subscriptionValidationResponse);
+        } else if(isCreateEvent){
+            log.info("blob created event");
+        } else if (isDeleteEvent){
+            log.info("blob deleted event");
         }
 
         return ResponseEntity.ok(HttpStatus.OK);
@@ -73,13 +81,13 @@ public class EventGridConsumerController {
 
     @PostMapping("/build")
     public ResponseEntity<?> build(@RequestBody List<Event> body, @RequestHeader HttpHeaders headers){
-        System.out.println("In build Function");
+        log.info("In build Function");
         printHeaders(headers);
         Gson g = new Gson();
-        System.out.println("JSON: " + g.toJson(body));
+        log.info("JSON: " + g.toJson(body));
         boolean isValidationEvent = body.get(0).getEventType().equalsIgnoreCase("Microsoft.Storage.BlobCreated");
         if (isValidationEvent) {
-            System.out.println("blob created or deleted event");
+            log.info("blob created or deleted event");
         }
         return ResponseEntity.ok(HttpStatus.OK);
     }
@@ -89,7 +97,7 @@ public class EventGridConsumerController {
 
         while (itr.hasNext()) {
             String key = itr.next().toString();
-            System.out.println("Key: " + key + ", Value: " + headers.get(key));
+            log.info("Key: " + key + ", Value: " + headers.get(key));
         }
     }
 }
